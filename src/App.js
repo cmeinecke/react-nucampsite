107c6596e065d7afff60c334a2dd3b25c324f5c7
Colin Meinecke <cmeinecke@gmail.com>
2022-05-07 12:58:24 -0500

    react workshop 1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import Header from './components/Header';
 import './App.css';
+import CampsitesList from './features/CampsitesList';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="App">
-      hello nucamp!
+    <div className='App'>
+      <Header />
+      <CampsitesList />
+      <Footer />
     </div>
   );
 }
