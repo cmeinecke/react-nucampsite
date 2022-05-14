2ec5f9aab2b2299f93a2bf55db1fbe68f722f466
Colin Meinecke <cmeinecke@gmail.com>
2022-05-13 21:02:55 -0500

    Added comments to campsite directory

--- src/features/campsites/CampsiteDetail.js
+++ src/features/campsites/CampsiteDetail.js
@@ -4,11 +4,11 @@ const CampsiteDetail = ({ campsite }) => {
     const { image, name, description } = campsite;
 
     return (
-        <Col md='12' className='m-4'>
+        <Col md='5' className='m-1'>
             <Card>
                 <CardImg top src={image} alt={name} />
                 <CardBody>
-                    <CardText>{description}</CardText>
+                    <CardText>{description} </CardText>
                 </CardBody>
             </Card>
         </Col>
